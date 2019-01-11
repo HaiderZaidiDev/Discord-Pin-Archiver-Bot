7f996e8997ed892e104f7d9d5304bedb3633cbe1
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-11 17:05:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ async def on_message(message):
     if message.content.startswith('+pinned'):
       pinned = list(await client.pins_from(message.channel))
       #await client.send_message(message.channel, pinned.content)
-      for message in pinned:
-        print(message.content)
+      
+      print(pinned)
 
 client.run(sys.argv[1])
 client.close()
