12c49bf40a3e085186ecfa5d200f425c712a524b
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-11 17:54:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ async def on_ready():
   print('Operational')
 
 @client.event
-async def on_message(message):
+async def on_message(message, -1):
+  pinnedMessages = []
   if message.author != client.user:
     if message.content.startswith('+pinned'):
       pinned = list(await client.pins_from(message.channel))
+      last_pin = pinned[-1]
       #await client.send_message(message.channel, pinned.content)
       
       for data in pinned:
-        print(data.content[-1])
+        pinnedMessages.append(data)
+      
+      print(pinnedMessages)
+        
 client.run(sys.argv[1])
 client.close()
   
