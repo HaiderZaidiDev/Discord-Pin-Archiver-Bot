52d30f2be452bca599043ecc936d5360798a6875
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-11 15:59:19 -0500

    Updated error with sysarg

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 
 client = discord.Client()
 
+@client.event def on_ready():
+  print('Operational')
+
 @client.event
 async def on_message(message):
   if message.author != client.user:
     if message.content.startswith('+pinned'):
       await client.send_message(message.channel, pins_from(message.channel))
 
-client.run(sysargv[1])
+client.run(sys.argv[1])
 client.close()
   
