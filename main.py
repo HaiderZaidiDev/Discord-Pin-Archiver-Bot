261b3ca91f3389853ffcc0b8374e2b6a6bf1ca2e
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-11 15:57:25 -0500

    Coded base of bot.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import discord
+import sys
 
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+    if message.content.startswith('+pinned'):
+      await client.send_message(message.channel, pins_from(message.channel))
+
+client.run(sysargv[1])
+client.close()
+  
