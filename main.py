05234294dd374ae1ae9d647889e0a4cbe643be6d
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-11 19:56:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,27 @@ async def on_message(message):
   if message.author != client.user:
     if message.content.startswith('+lastpin'):
       pinnedMessages = []
-      pinned = list(await client.pins_from(message.channel))
+      pinned = list(await client.pins_from(message.channel).message.)
 
       
       for data in pinned:
         pinnedMessages.append(data.content)
       
       lastPin = pinnedMessages[0]
-      emb = discord.Embed(description=lastPin, color = 0xcf1c43)
+      emb = discord.Embed(description= '__**Last Pinned Message**__: \n \n' + lastPin, color = 0xcf1c43)
       await client.send_message(message.channel, embed=emb)
     
     if message.content.startswith('+pinned'):
+      print(client.channel.name)
       pinnedMessages = []
       pinned = list(await client.pins_from(message.channel))
 
       for data in pinned:
         pinnedMessages.append(data.content)
       
-      desc=''
+      desc='__**Pinned Messages:**__ \n \n'
       for pins in pinnedMessages:
-        desc+= pins + '\n'  
+        desc+= pins + '\n \n'  
       
       emb = discord.Embed(description=desc, color = 0xcf1c43)
       await client.send_message(message.channel, embed=emb)
