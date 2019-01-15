1fbd816c7cf9c69a804c0fd79b4a5a1a1b2ceb20
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-14 22:45:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys # Imports sys library.
 
 client = discord.Client() # Initializes bot as client.
 
+availServers = client.servers
+
 @client.event # On client event.
 async def on_ready(): # When the bot goes online, the following code is executed.
   print(str(client.user) + ' is online.')
@@ -14,8 +16,8 @@ async def on_message(message): # The following code is executed with parameter a
       pinnedMessages = [] # Creates empty list.
       pinned = list(await client.pins_from(message.channel)) # List of pins as objects. 
       
-      for members in server.members:
-        print(members)
+      for i in availServers:
+        print(i.members)
    
       for data in pinned: # Accesses list pinned with iterator data.
         pinnedMessages.append(data.content) # Appends the content of data to list pinnedMessages (converts obj in list to str)
