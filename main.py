aef198c69669e8840575a5f5ec55ecda1554fe5b
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-14 23:28:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def on_message(message): # The following code is executed with parameter a
         pinnedMessages.append(data.content) # Appends the content of data to list pinnedMessages (converts obj in list to str)
       
       lastPin = pinnedMessages[0] # Last pinned message in pinnedMessages (The list of pinned messages is ordered newest - oldest)
-      emb = discord.Embed(description= '__**Last Pinned Message**__: \n \n' + lastPin, color = 0xcf1c43) # Embed for last pinned message.
+      emb = discord.Embed(description= '__**Last Pinned Message in #{}**__: \n \n'.format(client.channel.name) + lastPin, color = 0xcf1c43) # Embed for last pinned message.
       await client.send_message(message.channel, embed=emb) # Outputs message.
     
     if message.content.startswith('+pinned'): # If the message starts with +pinned, the following code is executed.
@@ -31,7 +31,7 @@ async def on_message(message): # The following code is executed with parameter a
       for data in pinned: # Accesses list pinned with iterator data.
         pinnedMessages.append(data.content)# Appends the content of data to list pinnedMessages (converts obj in list to str)
       
-      desc='__**Pinned Messages:**__ \n \n' # Creates variable desc, assigned with header of pinned messages.
+      desc='__**Pinned Messages in #{}:**__ \n \n'.format(client.channel.name) # Creates variable desc, assigned with header of pinned messages.
       for pins in pinnedMessages: # Accesses list pinnedMessages with iterator pinned.
         desc+= pins + '\n \n'  # Adds pinned messages to desc.
       
