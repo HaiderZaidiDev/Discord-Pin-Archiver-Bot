2ad65383357cc8df4857bf64795129b924d54896
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-20 23:04:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,10 @@ async def on_message(message): # The following code is executed with parameter a
       lastPin = pinnedMessages[0] # Last pinned message in pinnedMessages (The list of pinned messages is ordered newest - oldest)
       emb = discord.Embed(description= '__**Last Pinned Message in #{}**__: \n \n'.format(message.channel.name) + lastPin, color = 0xcf1c43) # Embed for last pinned message.
       await client.send_message(message.channel, embed=emb) # Outputs message.
-    
-    if message.content.startswith('+pinned'): # If the message starts with +pinned, the following code is executed.
-      pinned = list(await client.pins_from(message.channel)) # List of pins as objects.
 
-      for data in pinned: # Accesses list pinned with iterator data.
-        if data.content not in savedPins:
-          savedPins.append(data.content)# Appends the content of data to list pinnedMessages (converts obj in list to str)
-      
-
-      desc='__**Pinned Messages in #{}:**__ \n \n'.format(message.channel.name) # Creates variable desc, assigned with header of pinned messages.
-      for pins in savedPins: # Accesses list pinnedMessages with iterator pinned.
-        desc+= pins + '\n \n'  # Adds pinned messages to desc.
-      
-      emb = discord.Embed(description=desc, color = 0xcf1c43) # Embed for all pinned messages in current channel.
-      await client.send_message(message.channel, embed=emb) # Outputs all pinned messages in current channel.
-     
-
+@client.event
+async def on_message_edit(before):
+  print(before)
         
       
       
