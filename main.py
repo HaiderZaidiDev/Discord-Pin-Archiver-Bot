247c6078c015ebe55bd1ae690c083378356a0c92
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 17:35:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def on_message_edit(before, after): # The following code is executed on me
   x = await client.pins_from(before.channel) # Returns list of pins as message objects.
   pinnedContent = [message.content for message in x] # list of strings for message objects in x. 
  
-  if before.author != client.user and !before.author.bot in pinnedContent: # If the message was not sent by a bot, and is the last pinned message in the channel, the following code is executed.
+  if before.author != client.user and before.content in pinnedContent: # If the message was not sent by a bot, and is the last pinned message in the channel, the following code is executed.
     name = before.author.name # Name as author of message.
     avatar = before.author.avatar_url # Avatar as avatar url of message author.
     pinContent = before.content # pinContent as string of pinned message.
