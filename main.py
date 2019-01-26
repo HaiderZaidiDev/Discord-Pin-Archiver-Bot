5107016b20c9e10ed8019bc3f47f2c74a4601a78
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 13:32:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,10 @@ async def on_message(message): # The following code is executed on message event
     if message.content.startswith('+maintenance'):
       emb = discord.Embed(description = 'Pin Archiver is down for maintenance.', color = 0xcf1c43) # Initalizes embed with description pinContent.
       await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
-      
+    
+    if message.content.startswith('+unmaintenance'):
+      msgLogs = await client.logs_from(message.channel)
+      print(msgLogs)
     
 
 @client.event
