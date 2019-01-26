2034ddb73d67ed885f734629baeb0a6fc775f039
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 17:52:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ async def on_message(message): # The following code is executed on message event
       #await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
     
     if message.content.startswith('+unmaintenance'):
-      x = client.logs_from(message.channel)
-      msgs = [message.content for message in x] 
-      print(msgs)
+      x = yield from client.logs_from(message.channel)
+      for msgs in x:
+        print(x.content)
       
     
 
