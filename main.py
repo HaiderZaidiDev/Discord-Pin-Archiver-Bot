d27de09b8bfa70b5e35bb71980d68fb2917e0f8f
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 17:41:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,7 @@ async def on_message(message): # The following code is executed on message event
       #await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
     
     if message.content.startswith('+unmaintenance'):
-      if message.author.bot:
-        print('bot')
-      else:
+      if !message.author.bot:
         print('not bot')
         
       x = client.logs_from(message.channel)
