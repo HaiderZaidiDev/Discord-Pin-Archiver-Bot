5dabca00a35fce9377c0e2c252017fd54bc1e048
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 13:20:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ async def on_message(message): # The following code is executed on message event
       emb.set_author(name=pinnedNames[0], icon_url=pinnedAvatars[0]) # Sets the embeds avatar and name that matches to the corresponding information in x.
       await client.send_message(message.channel, embed=emb) # Sends message containing embed to channel message was executed in. 
     
+    if message.content.startswith('+maintenance'):
+      emb = discord.Embed(description='Pin Archiver is down for maintenance.', color = 0xcf1c43)
+      await client.send_message(discord.Object(id='538545784497504276'), embed = emb)
+      
+    
 
 @client.event
 async def on_message_edit(before, after): # The following code is executed on message edit even (whenever a message is pinned/edited).
@@ -38,7 +43,7 @@ async def on_message_edit(before, after): # The following code is executed on me
     emb = discord.Embed(description = pinContent, color = 0xcf1c43) # Initalizes embed with description pinContent.
     emb.set_author(name=name, icon_url=avatar) # Sets author and avatar url of the author of pinned message.
     emb.set_footer(text= before.created_at + 'Sent in #{}'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
-    await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
+    await client.send_message(discord.Object(id='536761750242983937'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
 
       
      
