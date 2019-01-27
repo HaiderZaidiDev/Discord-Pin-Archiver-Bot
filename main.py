2b4fb085dcdc7f5c8ef3afbe4214d801b637fc6d
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-26 19:26:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,8 @@ async def on_message(message): # The following code is executed on message event
       #await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
     
     if message.content.startswith('+del'):
-      
-      for i in range(len(message.author.roles)):
-        if str('@everyone') in message.author.roles:
-          print('yes')
-        else:
-          print('no')
+      userRoles = [x for roles in message.author.roles]
+      print(userRoles)
         
       
       #async for message in client.logs_from(discord.Object(id='536761750242983937'), limit = 1):
