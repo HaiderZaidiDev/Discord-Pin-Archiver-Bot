ac8dc4000aeb079dc95960f2d5b20d688b51d40b
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-28 16:55:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,21 +24,21 @@ async def on_message(message): # The following code is executed on message event
       emb.set_author(name=pinnedNames[0], icon_url=pinnedAvatars[0]) # Sets the embeds avatar and name that matches to the corresponding information in x.
       await client.send_message(message.channel, embed=emb) # Sends message containing embed to channel message was executed in. 
     
-    if message.content.startswith('+maintenance') and message.author.id == '357652932377837589':
+    if message.content.startswith('+maintenance') and message.author.id == '357652932377837589': #If the message starts with +maintenance, and was made by user @Nitr0us#5090, the following code is executed:
       emb = discord.Embed(description = 'Pin Archiver is down for maintenance.', color = 0xcf1c43) # Initalizes embed with description pinContent.
       await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
     
-    if message.content.startswith('+ping'):
-      emb = discord.Embed(description = 'Online.', color = 0xcf1c43) # Intilializes embed with description as index 0 of pinnedContent
+    if message.content.startswith('+ping'): # If the message starts with +ping, the following code is executed.
+      emb = discord.Embed(description = 'Online.', color = 0xcf1c43) # Intilializes embed with online message.
       await client.send_message(message.channel, embed=emb) # Sends message containing embed to channel message was executed in. 
       
-    if message.content.startswith('+del'):
-      if str('Administrator') in userRoles or str('Moderator') in userRoles or message.author.id == '357652932377837589':
-        async for message in client.logs_from(discord.Object(id='538545784497504276'), limit = 1):
-          lastMessage = message
-        await client.delete_message(lastMessage)
+    if message.content.startswith('+del'): # If the message starts with +del, the following code is executed.
+      if str('Administrator') in userRoles or str('Moderator') in userRoles or message.author.id == '357652932377837589': # If the user is an Administrator, Moderator or @Nitr0us#5090 the following code is executed.
+        async for message in client.logs_from(discord.Object(id='538545784497504276'), limit = 1): # Fetches last message in the channel #pin-archive
+          lastMessage = message # Variable for last message sent in #pin-archive
+        await client.delete_message(lastMessage) # Deletes lastMessage.
     
-    if message.content.startswith('+help'):
+    if message.content.startswith('+help'): # If the message starts with +help, the following code is executed.
       helpMsg = '''
        __**Information**__:
         
@@ -62,15 +62,15 @@ async def on_message(message): # The following code is executed on message event
         Usage: +del
         Permisssion: Administrators & Moderators
         Purpose: To delete the last pinned message in #pin-archive.
-      '''
-      emb = discord.Embed(description=helpMsg, color = 0xcf1c43)
-      await client.send_message(message.channel, embed=emb)
+      ''' 
+      emb = discord.Embed(description=helpMsg, color = 0xcf1c43) # Intializes embed with help message as description.
+      await client.send_message(message.channel, embed=emb) # Sends message containing embed to the channel the command was executed in.
      
 @client.event
 async def on_message_edit(before, after): # The following code is executed on message edit even (whenever a message is pinned/edited).
   x = await client.pins_from(before.channel) # Returns list of pins as message objects.
-  pinnedContent = [message.content for message in x] # list of strings for message objects in x. 
-  attachments = before.attachments
+  pinnedContent = [message.content for message in x] # List of strings for message objects in x. 
+  attachments = before.attachments # Returns list of message attachments in dictionaries.
 
   
   if before.author != client.user and before.content in pinnedContent and before.author.bot == False: # If the message was not sent by a bot, and is the last pinned message in the channel, the following code is executed.
@@ -82,9 +82,9 @@ async def on_message_edit(before, after): # The following code is executed on me
     emb = discord.Embed(description = pinContent, color = 0xcf1c43) # Initalizes embed with description pinContent.
     emb.set_author(name=name, icon_url=avatar) # Sets author and avatar url of the author of pinned message.
     
-    if attachments != []:
-      imgContent = attachments[0]['url']
-      emb.set_image(url=imgContent)
+    if attachments != []: # If the pinned message has an attachment, the following code is executed.
+      imgContent = attachments[0]['url'] # Gets url of the attachment.
+      emb.set_image(url=imgContent) # Sets image url as embed image.
       
     emb.set_footer(text='Sent in #{}'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
     await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
