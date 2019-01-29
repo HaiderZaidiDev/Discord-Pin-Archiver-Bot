ed45be59136dabf4f79e2bdf4fa2de25970c3436
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-28 21:52:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def on_message(message): # The following code is executed on message event
       emb.set_author(name=pinnedNames[0], icon_url=pinnedAvatars[0]) # Sets the embeds avatar and name that matches to the corresponding information in x.
       
       if attachments != []: # If the pinned message has an attachment, the following code is executed.
-        imgContent = attachments[0]['url'] # Gets url of the attachment.
+        imgContent = attachments[1]['url'] # Gets url of the attachment.
         emb.set_image(url=imgContent) # Sets image url as embed image.
         
       await client.send_message(message.channel, embed=emb) # Sends message containing embed to channel message was executed in. 
