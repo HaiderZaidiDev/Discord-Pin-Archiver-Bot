1261817852cd74f42b259430da83dd8885d760e5
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-30 22:21:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -74,13 +74,27 @@ async def on_message(message): # The following code is executed on message event
           lastMessage = message # Variable for last message sent in #pin-archive
         await client.delete_message(lastMessage) # Deletes lastMessage.
     
-    if message.content.startswith('+pin') and message.content != str('+ping'):
+    if message.content.startswith('+archive'):
       if str('Administrator') in userRoles or str('Moderator') in userRoles or message.author.id == '357652932377837589': # If the user is an Administrator, Moderator or @Nitr0us#5090 the following code is executed.
-        x = await client.pins_from(message.channel) # Returns list of pins as message objects.
-        pinnedIds = [message.id for message in x] # List of strings for message objects in x.
-        msgIdToPin = message.content.replace('+pin ', '')
-        msg = await client.get_message(message.channel, msgIdToPin)
-        on_message_edit(msg)
+        msgIdToArchive = message.content.replace('+archive ', '')
+        msg = await client.get_message(message.channel, msgIdToArchive)
+        attachments = msg.attachments # Returns list of message attachments in dictionaries.
+
+        name = msg.author.name # Name as author of message.
+        avatar = msg.author.avatar_url # Avatar as avatar url of message author.
+        pinContent = msg.content # pinContent as string of pinned message.
+        msgChannel = msg.channel # msgChannel as channel name the message was pinned in.
+   
+        emb = discord.Embed(description = pinContent, color = 0xcf1c43) # Initalizes embed with description pinContent.
+        emb.set_author(name=name, icon_url=avatar) # Sets author and avatar url of the author of pinned message.
+    
+        if attachments != []: # If the pinned message has an attachment, the following code is executed.
+          imgContent = attachments[0]['url'] # Gets url of the attachment.
+          emb.set_image(url=imgContent) # Sets image url as embed image.
+      
+        emb.set_footer(text='Sent in #{}'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
+        await client.send_message(discord.Object(id='536761750242983937'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
+
         
         
 
