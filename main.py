82aeeb470b3d7799763245f87add08bb634e28cb
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-30 21:31:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,35 @@ client = discord.Client() # Initializes bot as client.
 @client.event 
 async def on_ready(): # When the bot goes online, the following code is executed.
   print(str(client.user) + ' is online.') # Prints operaitonal message.
+     
+@client.event
+async def on_message_edit(before, after): # The following code is executed on message edit even (whenever a message is pinned/edited).
+  x = await client.pins_from(before.channel) # Returns list of pins as message objects.
+  pinnedContent = [message.content for message in x] # List of strings for message objects in x. 
+  attachments = before.attachments # Returns list of message attachments in dictionaries.
 
+  if before.author != client.user and before.content in pinnedContent and before.author.bot == False and before.content != '': # If the message was not sent by a bot, and is the last pinned message in the channel, the following code is executed.
+    name = before.author.name # Name as author of message.
+    avatar = before.author.avatar_url # Avatar as avatar url of message author.
+    pinContent = before.content # pinContent as string of pinned message.
+    msgChannel = before.channel # msgChannel as channel name the message was pinned in.
+   
+    emb = discord.Embed(description = pinContent, color = 0xcf1c43) # Initalizes embed with description pinContent.
+    emb.set_author(name=name, icon_url=avatar) # Sets author and avatar url of the author of pinned message.
+    
+    if attachments != []: # If the pinned message has an attachment, the following code is executed.
+      imgContent = attachments[0]['url'] # Gets url of the attachment.
+      emb.set_image(url=imgContent) # Sets image url as embed image.
+      
+    emb.set_footer(text='Sent in #{}'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
+    await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
 
+@client.event
+async def on_reaction_add(reaction, user):
+  if reaction.emoji == '📌':
+    if reaction.count >= 7:
+      await client.pin_message(reaction.message)
+ 
 @client.event
 async def on_message(message): # The following code is executed on message event, parameter message.
   userRoles = [role.name for role in message.author.roles]
@@ -85,34 +112,6 @@ async def on_message(message): # The following code is executed on message event
       ''' 
       emb = discord.Embed(description=helpMsg, color = 0xcf1c43) # Intializes embed with help message as description.
       await client.send_message(message.channel, embed=emb) # Sends message containing embed to the channel the command was executed in.
-     
-@client.event
-async def on_message_edit(before, after): # The following code is executed on message edit even (whenever a message is pinned/edited).
-  x = await client.pins_from(before.channel) # Returns list of pins as message objects.
-  pinnedContent = [message.content for message in x] # List of strings for message objects in x. 
-  attachments = before.attachments # Returns list of message attachments in dictionaries.
-
-  if before.author != client.user and before.content in pinnedContent and before.author.bot == False and before.content != '': # If the message was not sent by a bot, and is the last pinned message in the channel, the following code is executed.
-    name = before.author.name # Name as author of message.
-    avatar = before.author.avatar_url # Avatar as avatar url of message author.
-    pinContent = before.content # pinContent as string of pinned message.
-    msgChannel = before.channel # msgChannel as channel name the message was pinned in.
-   
-    emb = discord.Embed(description = pinContent, color = 0xcf1c43) # Initalizes embed with description pinContent.
-    emb.set_author(name=name, icon_url=avatar) # Sets author and avatar url of the author of pinned message.
-    
-    if attachments != []: # If the pinned message has an attachment, the following code is executed.
-      imgContent = attachments[0]['url'] # Gets url of the attachment.
-      emb.set_image(url=imgContent) # Sets image url as embed image.
-      
-    emb.set_footer(text='Sent in #{}'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
-    await client.send_message(discord.Object(id='538545784497504276'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
-
-@client.event
-async def on_reaction_add(reaction, user):
-  if reaction.emoji == '📌':
-    if reaction.count >= 7:
-      await client.pin_message(reaction.message)
       
   
 client.run(sys.argv[1]) # Runs bot with token as system argument. 
