da5699f943cc811f3945387ea55a8de1336b0cab
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-02-09 23:14:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def on_message_edit(before, after): # The following code is executed on me
       imgContent = attachments[0]['url'] # Gets url of the attachment.
       emb.set_image(url=imgContent) # Sets image url as embed image.
       
-    emb.set_footer(text='[Sent in #{}](https://discordapp.com)'.format(msgChannel)) # Sets footer as the channel the message was sent and pinned in.
+    emb.set_footer(text='[Sent in #{}](https://discordapp.com)') # Sets footer as the channel the message was sent and pinned in.
     await client.send_message(discord.Object(id='536761750242983937'), embed=emb) # Sends message containing embed to specified channel (presumably a log channel i.e #pins-archive).
 
 @client.event
