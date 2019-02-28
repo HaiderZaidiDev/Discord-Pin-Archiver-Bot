1ed495ade14ce57b375211aabe3c4ad1b9848cf8
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-02-27 21:57:28 -0500

    Updated main.py
    hotfix test for +lastpin attachment handling

--- main.py
+++ main.py
@@ -94,10 +94,11 @@ async def on_message(message):
     if message.author != client.user:
         if message.content.startswith('+lastpin'):
             x = await client.pins_from(message.channel)
-            pinned_names = [message.author.display_name for message in x]
-            pinned_avatars = [message.author.avatar_url for message in x]
-            pinned_content = [message.content for message in x]
-            attachments = [message.attachments for message in x]
+            lastPin = x[0]
+            pinned_names = lastPin.author.display_name 
+            pinned_avatars = lastPin.author.avatar_url 
+            pinned_content = lastPin.content
+            attachments = lastPin.attachments
 
             # Description is the contents of the first pinned message
             emb = discord.Embed(description=pinned_content[0], color=0x7289da)
@@ -111,7 +112,7 @@ async def on_message(message):
             # Handle attachments in pins
             if attachments:
                 print(attachments)
-                img_content = attachments[0]['url']
+                img_content = attachments[0][0]['url']
                 emb.set_image(url=img_content)
 
             await client.send_message(message.channel, embed=emb)
